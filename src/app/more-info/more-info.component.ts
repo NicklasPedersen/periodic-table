8ab409e514ca1f2b28883278cb55f1e68e0183ec
Nicklas Pedersen (nick8186) <nick8186@zbc.dk>
2020-08-17 10:50:01 +0200

    a

--- src/app/more-info/more-info.component.ts
+++ src/app/more-info/more-info.component.ts
@@ -9,7 +9,6 @@ import { Atom } from '../atom.model';
   styleUrls: ['./more-info.component.css']
 })
 export class MoreInfoComponent implements OnInit {
-  id: number;
   atom: Atom = new Atom();
   constructor(
     private route: ActivatedRoute,
@@ -17,9 +16,9 @@ export class MoreInfoComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.id = +params['id'];
       this.dataService.refreshData(res => {
-        this.atom = res[this.id];
+        // we get an id based on atomicNumber so we have to transpose that range from 1-118 to 0-117 by subtracting 1
+        this.atom = res[params['id'] - 1];
       });
     });
   }
